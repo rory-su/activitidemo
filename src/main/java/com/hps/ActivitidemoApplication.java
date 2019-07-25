033ae27b95bf1927cab43e8a1ca8738f0588008f
rory-su <136471676@qq.com>
2019-07-25 12:16:28 +0800

    jj

--- src/main/java/com/hps/ActivitidemoApplication.java
+++ src/main/java/com/hps/ActivitidemoApplication.java
@@ -3,8 +3,9 @@ package com.hps;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.activiti.spring.boot.SecurityAutoConfiguration.class,SecurityAutoConfiguration.class})
 @MapperScan("com.hps.dao")
 public class ActivitidemoApplication {
 
