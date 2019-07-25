f43997da93cb72329d01512d82c808fda2b26927
rory-su <136471676@qq.com>
2019-07-25 11:35:00 +0800

    rr

--- src/main/java/com/hps/ActivitidemoApplication.java
+++ src/main/java/com/hps/ActivitidemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.hps.dao")
 public class ActivitidemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ActivitidemoApplication.class, args);
     }
