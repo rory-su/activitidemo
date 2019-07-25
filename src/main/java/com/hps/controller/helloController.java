d32647b5316fbb30c6c046615d1bb8cc6d449517
rory-su <136471676@qq.com>
2019-07-25 12:19:26 +0800

    jj

--- src/main/java/com/hps/controller/helloController.java
+++ src/main/java/com/hps/controller/helloController.java
@@ -2,13 +2,18 @@ package com.hps.controller;
 
 
 
+import org.activiti.engine.IdentityService;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
+
+
     @RequestMapping("hello")
     public String hello(){
+
         return "hello";
     }
 }
